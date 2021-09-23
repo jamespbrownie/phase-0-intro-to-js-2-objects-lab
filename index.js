fbbc07d54f6776ec0d0fdad75a670e1e09941556
James Brownie <jamespbrownie@gmail.com>
2021-09-23 12:57:48 -0500

    commit tree 2411662aac774a4f1eca7ffedae8ac309c28d9a7

--- index.js
+++ index.js
@@ -1,28 +1,65 @@
 // Write your solution in this file!
-
 const employee = {
-    name: "1",
+    name: "2",
     streetAddress: "2"
 }
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
+function updateEmployeeWithKeyAndValue (employee, key, value) {
     const newEmployee = {...employee}
     newEmployee[key] = value;
     return newEmployee
 }
- 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value
     return employee
-} 
+}
 
 function deleteFromEmployeeByKey(employee, key) {
     const newEmployee = {...employee} 
-    delete newEmployee[key]
+    delete newEmployee[key] 
     return newEmployee
 }
 
-function destructivelyDeleteFromEmployeeByKey (employee, key) {
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key]
     return employee
 }
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {
+//     name: "1",
+//     streetAddress: "2"
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     const newEmployee = {...employee}
+//     newEmployee[key] = value;
+//     return newEmployee
+// }
+ 
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value;
+//     return employee
+// } 
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newEmployee = {...employee} 
+//     delete newEmployee[key]
+//     return newEmployee
+// }
+
+// function destructivelyDeleteFromEmployeeByKey (employee, key) {
+//     delete employee[key]
+//     return employee
+// }
